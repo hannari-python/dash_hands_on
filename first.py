f33f3b5c9f8a33cdb507e646ee7c0e8b5d2a13c0
Hideyuki Ogawa <mazarimono@gmail.com>
2020-03-13 15:29:28 +0900

    first commit

---
+++ first.py
@@ -0,0 +1,32 @@
+import dash  
+import dash_core_components as dcc  
+import dash_html_components as html 
+from dash.dependencies import Input, Output 
+
+import plotly.express as px  
+
+gapminder = px.data.gapminder()
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+
+    html.H1("Gapminder"),
+
+    dcc.Dropdown(id="first-dropdown", 
+        options=[{"value": i, "label": i} for i in gapminder.country.unique()],
+        value="Japan"
+    ),
+    dcc.Graph(id="select_graph")
+
+])
+
+@app.callback(Output("select_graph", "figure"),
+            [Input("first-dropdown", "value")])
+def update_graph(selected_value):
+    dff = gapminder[gapminder["country"]==selected_value]
+    return px.line(dff, x="year", y="pop")
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
