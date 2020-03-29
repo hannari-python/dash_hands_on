68efc8ccdfa1d94db00eec960e2ee233ce6291ee
Hideyuki Ogawa <mazarimono@gmail.com>
2020-03-29 10:50:24 +0900

    black to hands_on_02

--- hands_on_02/today_app.py
+++ hands_on_02/today_app.py
@@ -1,7 +1,7 @@
-import dash  
-import dash_core_components as dcc 
-import dash_html_components as html  
-import plotly.express as px 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
 
 # データセットの読み込み
 
@@ -13,60 +13,142 @@ gap_2007 = gapminder[gapminder["year"] == 2007]
 
 jp_gdp = px.line(jp_data, x="year", y="gdpPercap")
 
-world_2007 = px.scatter(gap_2007, x="gdpPercap", y="lifeExp", size="pop", color="continent", log_x=True, log_y=True, size_max=80)
+world_2007 = px.scatter(
+    gap_2007,
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    log_x=True,
+    log_y=True,
+    size_max=80,
+)
 
-gap_map = px.scatter_geo(gapminder, locations="iso_alpha", size="gdpPercap", color="continent", animation_frame="year")
+gap_map = px.scatter_geo(
+    gapminder,
+    locations="iso_alpha",
+    size="gdpPercap",
+    color="continent",
+    animation_frame="year",
+)
 
 # Dashインスタンスを作成する
 app = dash.Dash(__name__)
 
 # レイアウトを渡す
-app.layout = html.Div([
-
-html.H1("はんなりPython Dash ハンズオン レイアウト編", style={"textAlign": "center", "color": "red", "padding": "3%", "backgroundColor": "#cff09e"}),
-
-html.Div([
-
-html.Div([
-html.H2("はんなりPython"),
-html.Img(src="assets/hannari.png", style={"width": "100%"}),
-], style={"width": "48%", "padding": "1%", "display": "inline-block", "backgroundColor": "#FBFFB9"}),
-
-html.Div([
-
-
-
-    html.Div(style={"borderRadius": "50%", "backgroundColor":"lime", "height": 200, "width": 200, "display": "inline-block"}),
-
- 
-        html.Button("ボタン", style={"borderRadius": "50%", "backgroundColor": "red", "height":100, "width": 100, "display": "inline-block", "verticalAlign": "center"}),
-    
-        html.Button("ボタン", style={"borderRadius": "50%", "backgroundColor": "red", "height":50, "width": 50, "display": "inline-block", "verticalAlign": "top"}),
-
-
-    dcc.Markdown("""マークダウン     
+app.layout = html.Div(
+    [
+        html.H1(
+            "はんなりPython Dash ハンズオン レイアウト編",
+            style={
+                "textAlign": "center",
+                "color": "red",
+                "padding": "3%",
+                "backgroundColor": "#cff09e",
+            },
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.H2("はんなりPython"),
+                        html.Img(src="assets/hannari.png", style={"width": "100%"}),
+                    ],
+                    style={
+                        "width": "48%",
+                        "padding": "1%",
+                        "display": "inline-block",
+                        "backgroundColor": "#FBFFB9",
+                    },
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            style={
+                                "borderRadius": "50%",
+                                "backgroundColor": "lime",
+                                "height": 200,
+                                "width": 200,
+                                "display": "inline-block",
+                            }
+                        ),
+                        html.Button(
+                            "ボタン",
+                            style={
+                                "borderRadius": "50%",
+                                "backgroundColor": "red",
+                                "height": 100,
+                                "width": 100,
+                                "display": "inline-block",
+                                "verticalAlign": "center",
+                            },
+                        ),
+                        html.Button(
+                            "ボタン",
+                            style={
+                                "borderRadius": "50%",
+                                "backgroundColor": "red",
+                                "height": 50,
+                                "width": 50,
+                                "display": "inline-block",
+                                "verticalAlign": "top",
+                            },
+                        ),
+                        dcc.Markdown(
+                            """マークダウン     
         マークダウンはdash_core_componentsに含まれる
-    """, style={"fontSize": 20, "color": "yellow", "backgroundColor": "black", "padding":"2%"}),
-    dcc.Slider(
-        min=-20, max=100, value=30, marks={i: f"{i}" for i in range(-20,101, 20)}
-    ),
-
-
-
-], style={"width": "48%", "padding": "1%", "display": "inline-block", "verticalAlign": "top", "backgroundColor": "#a5dff9"})
-
-]),
-
-html.Div([
-    dcc.Tabs([
-        dcc.Tab(label="label One", value="label One", children=dcc.Graph(figure=jp_gdp)),
-        dcc.Tab(label="label Two", value="label Two", children=dcc.Graph(figure=world_2007)),
-        dcc.Tab(label="label Three", value="label Three", children=dcc.Graph(figure=gap_map))
-    ], value="label One")
-]),
-
-], style={"backgroundColor": "#FFEEE4", "padding": "2%"})
+    """,
+                            style={
+                                "fontSize": 20,
+                                "color": "yellow",
+                                "backgroundColor": "black",
+                                "padding": "2%",
+                            },
+                        ),
+                        dcc.Slider(
+                            min=-20,
+                            max=100,
+                            value=30,
+                            marks={i: f"{i}" for i in range(-20, 101, 20)},
+                        ),
+                    ],
+                    style={
+                        "width": "48%",
+                        "padding": "1%",
+                        "display": "inline-block",
+                        "verticalAlign": "top",
+                        "backgroundColor": "#a5dff9",
+                    },
+                ),
+            ]
+        ),
+        html.Div(
+            [
+                dcc.Tabs(
+                    [
+                        dcc.Tab(
+                            label="label One",
+                            value="label One",
+                            children=dcc.Graph(figure=jp_gdp),
+                        ),
+                        dcc.Tab(
+                            label="label Two",
+                            value="label Two",
+                            children=dcc.Graph(figure=world_2007),
+                        ),
+                        dcc.Tab(
+                            label="label Three",
+                            value="label Three",
+                            children=dcc.Graph(figure=gap_map),
+                        ),
+                    ],
+                    value="label One",
+                )
+            ]
+        ),
+    ],
+    style={"backgroundColor": "#FFEEE4", "padding": "2%"},
+)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
-    
